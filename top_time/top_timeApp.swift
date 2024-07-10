0b62550f5397d4a4bb9bfffefae4da426e0df0d6
leegyoungmin <20160921@vision.hoseo.edu>
2024-07-10 10:01:39 +0900

    Initial Commit

---
+++ top_time/top_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  top_timeApp.swift
+//  top_time
+//
+//  Created by 이경민 on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct top_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
